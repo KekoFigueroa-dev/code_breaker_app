da51b3b72688425f8d879b72a0cfa1699072ec6c
KekoFigueroa-dev <thenaturalistgospel@gmail.com>
2025-08-03 13:45:23 -0600

    works with hardcoded phrase

--- codebraker.py
+++ codebraker.py
@@ -1,1 +1,59 @@
+from collections import Counter
 #code breaker app
+
+# Welcome message
+print("Welcome to my Code Breaker App!")
+
+#List elements to remove non-letter characters
+non_letter_chars = " 0123456789.,;:!?'\"()[]{}<>-_=+`~@#$%^&*|\\/\n\t"
+# Hardcoded key phrases
+key_phrase_1 = """
+To Sherlock Holmes she is always the woman. I have seldom heard him mention her
+under any other name.
+In his eyes she eclipses and predominates the whole of her sex. It was not that
+he felt any emotion akin to love for Irene Adler.
+All emotions, and that one particularly, were abhorrent to his cold, precise but
+admirably balanced mind.
+He was, I take it, the most perfect reasoning and observing machine that the
+world has seen, but as a lover he would have placed himself in a false position.
+He never spoke of the softer passions, save with a gibe and a sneer.
+They were admirable things for the observer excellent for drawing the veil from
+men's motives and actions.
+But for the trained reasoner to admit such intrusions into his own delicate and
+finely adjusted temperament was to introduce a distracting factor which might throw a doubt upon all his mental results.
+Grit in a sensitive instrument, or a crack in one of his own highpower lenses, would not be more disturbing than a strong emotion in a nature such as his.
+And yet there was but one woman to him, and that woman was the late Irene Adler, of dubious and questionable memory.
+I had seen little of Holmes lately. My marriage had drifted us away from each
+other. My own complete happiness, and the homecentred interests which rise up around the man who first finds himself master of his own establishment,
+were sufficient to absorb all my attention, while Holmes, who loathed every form
+of society with his whole Bohemian soul,
+remained in our lodgings in Baker Street, buried among his old books, and
+alternating from week to week between cocaine and ambition,
+the drowsiness of the drug, and the fierce energy of his own keen nature.
+He was still, as ever, deeply attracted by the study of crime, and occupied his immense faculties and extraordinary powers of observation in
+following out those clues, and clearing up those mysteries which had been abandoned as hopeless by the
+official police.
+From time to time I heard some vague account of his doings: of his summons to
+Odessa in the case of the Trepoff murder, of his clearing up of the singular tragedy of the Atkinson brothers at
+Trincomalee, and finally of the mission which he had accomplished so delicately and
+successfully for the reigning family of Holland. Beyond these signs of his activity, however, which I merely shared with all the
+readers of the daily press, I knew little of my former friend and companion. """
+key_phrase_1 = key_phrase_1.lower()
+
+#Removing all non-letter characters
+for non_letter_char in non_letter_chars:
+    key_phrase_1 = key_phrase_1.replace(non_letter_char, '')
+
+total_occurrences = len(key_phrase_1)
+
+#Create a Counter for each occurrence of each letter
+letter_counter = Counter(key_phrase_1)
+
+#Determine the frequency of each letter
+print(f"\nFrequency analysis for: 'key_phrase_1'")
+print("\nLetter\tOccurrence\tPercentage")
+for key, value in sorted(letter_counter.items()):
+    percentage = 100*value/total_occurrences
+    percentage = round(percentage, 2)
+    print(f"{key}\t{value}\t\t{percentage}%")
+
