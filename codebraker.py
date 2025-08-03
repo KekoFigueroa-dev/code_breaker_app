36a582d7cb64cd601bf1fe2608549ccb82392826
KekoFigueroa-dev <thenaturalistgospel@gmail.com>
2025-08-03 13:47:58 -0600

    Second hardcoded mssg init

--- codebraker.py
+++ codebraker.py
@@ -21,10 +21,10 @@ They were admirable things for the observer excellent for drawing the veil from
 men's motives and actions.
 But for the trained reasoner to admit such intrusions into his own delicate and
 finely adjusted temperament was to introduce a distracting factor which might throw a doubt upon all his mental results.
-Grit in a sensitive instrument, or a crack in one of his own highpower lenses, would not be more disturbing than a strong emotion in a nature such as his.
+Grit in a sensitive instrument, or a crack in one of his own high power lenses, would not be more disturbing than a strong emotion in a nature such as his.
 And yet there was but one woman to him, and that woman was the late Irene Adler, of dubious and questionable memory.
 I had seen little of Holmes lately. My marriage had drifted us away from each
-other. My own complete happiness, and the homecentred interests which rise up around the man who first finds himself master of his own establishment,
+other. My own complete happiness, and the home centred interests which rise up around the man who first finds himself master of his own establishment,
 were sufficient to absorb all my attention, while Holmes, who loathed every form
 of society with his whole Bohemian soul,
 remained in our lodgings in Baker Street, buried among his old books, and
@@ -34,10 +34,11 @@ He was still, as ever, deeply attracted by the study of crime, and occupied his 
 following out those clues, and clearing up those mysteries which had been abandoned as hopeless by the
 official police.
 From time to time I heard some vague account of his doings: of his summons to
-Odessa in the case of the Trepoff murder, of his clearing up of the singular tragedy of the Atkinson brothers at
+Odessa in the case off murder, of his clearing up of the singular tragedy of the Atkinson brothers at
 Trincomalee, and finally of the mission which he had accomplished so delicately and
 successfully for the reigning family of Holland. Beyond these signs of his activity, however, which I merely shared with all the
 readers of the daily press, I knew little of my former friend and companion. """
+
 key_phrase_1 = key_phrase_1.lower()
 
 #Removing all non-letter characters
